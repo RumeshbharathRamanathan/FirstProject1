b4708c01c0d8263a89d4acee462b5fba9eaa2348
RumeshbharathRamanathan <rumeshbharathramanathan@gmail.com>
2026-07-21 14:12:17 +0530

    Update main.java

--- main.java
+++ main.java
@@ -3,6 +3,6 @@ public class Main {
     int x = 15;
     int y = 16;
     int sum = x + y;
-    System.out.println(sum); // Print the sum of x + y
+    System.out.println("test "+sum); // Print the sum of x + y
   }
 }
